f17091b81e5fce5c26c3cc097c7890b46318635d
ZHENGXIN\pc <talentzzx@gmail.com>
2018-02-09 20:17:14 +0800

    Address.java: extract class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -10,14 +10,17 @@ public class Address {
 
     public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must contain the following fields: "
-                                                             + "BLOCK, STREET, UNIT, POSTAL_CODE\n"
+            + "BLOCK, STREET, UNIT, POSTAL_CODE\n"
     public static final String ADDRESS_VALIDATION_REGEX = ".+,.+,.+,.+";
     public static final String ADDRESS_SPLIT_DELIMITER = ",";
 
     private static final int BLOCK_INDEX = 0;
-+   private static final int STREET_INDEX = 1;
-+   private static final int UNIT_INDEX = 2;
-+   private static final int POSTALCODE_INDEX = 3;
++
+    private static final int STREET_INDEX = 1;
++
+    private static final int UNIT_INDEX = 2;
++
+    private static final int POSTALCODE_INDEX = 3;
 
     public final String value;
     public Block BlockNumber;
@@ -71,55 +74,8 @@ public class Address {
     public boolean isPrivate() {
         return isPrivate;
     }
-
-
-    class Block {
-        private final String BlockNumber;
-
-        public Block( String Block_Number){
-            BlockNumber = Block_Number;
-     }
-         public String getBlock(){
-             return BlockNumber;
-        }
-    }
-
-    class Street {
-
-        private final String StreetName;
-
-        public Street( String Street_Name){
-            StreetName = Street_Name;
-        }
-
-        public String getStreet(){
-            return StreetName;
-        }
-    }
-
-    class Unit {
-
-        private final String UnitNumber;
-
-        public Unit( String Unit_Number){
-            UnitNumber = Unit_Number;
-        }
-
-        public String getUnit(){
-            return UnitNumber;
-        }
-    }
-
-    class Postal_Code {
-
-        private final String code;
-
-        public Block( String Pcode){
-            code = Pcode;
-        }
-
-        public String getPostalCode(){
-            return code;
-        }
-    }
 }
+    
+
+    
+   
