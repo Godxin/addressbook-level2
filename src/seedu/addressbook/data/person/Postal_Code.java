01fb8ba1c97f2e31a905ff024eb87cbc95f54238
ZHENGXIN\pc <talentzzx@gmail.com>
2018-02-09 20:19:18 +0800

    data/person: address split to four classes

---
+++ src/seedu/addressbook/data/person/Postal_Code.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public  class Postal_Code {
+
+    private final String code;
+
+    public Postal_Code( String Pcode){
+
+        code = Pcode;
+    }
+    public String getPostalCode(){
+
+        return code;
+    }
+}
---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+
+    private final String StreetName;
+
+    public Street(String Street_Name) {
+
+        StreetName = Street_Name;
+    }
+
+    public String getStreet() {
+
+        return StreetName;
+    }
+}
