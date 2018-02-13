38e01e97fb94c7351b2329fa4ac843fd0c0eb446
ZHENGXIN\pc <talentzzx@gmail.com>
2018-02-14 01:01:44 +0800

    FindCommandTest.java: update

--- test/java/seedu/addressbook/commands/FindCommandTest.java
+++ test/java/seedu/addressbook/commands/FindCommandTest.java
@@ -25,8 +25,8 @@ public class FindCommandTest {
         //same word, same case: matched
         assertFindCommandBehavior(new String[]{"Amy"}, Arrays.asList(td.amy));
 
-        //same word, different case: not matched
-        assertFindCommandBehavior(new String[]{"aMy"}, Collections.emptyList());
+        //same word, different case: matched
+        assertFindCommandBehavior(new String[]{"aMy"}, Arrays.asList(td.amy));
 
         //partial word: not matched
         assertFindCommandBehavior(new String[]{"my"}, Collections.emptyList());
@@ -38,8 +38,8 @@ public class FindCommandTest {
         //repeated keywords: matched
         assertFindCommandBehavior(new String[]{"Amy", "Amy"}, Arrays.asList(td.amy));
 
-        //Keyword matching a word in address: matched
-        assertFindCommandBehavior(new String[]{"Clementi"}, Arrays.asList(td.amy));
+        //Keyword matching a word in address: not matched
+        assertFindCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
     }
 
     /**
